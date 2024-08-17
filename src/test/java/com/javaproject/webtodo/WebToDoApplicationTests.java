79a7e19d45a870d0257982879569227bfe9efc21
${GITHUB_USER} <>
2024-08-17 10:24:58 -0300

    Config

---
+++ src/test/java/com/javaproject/webtodo/WebToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaproject.webtodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebToDoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
